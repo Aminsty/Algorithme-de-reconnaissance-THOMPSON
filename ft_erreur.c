312c01c0756d5e8c30cab8cbd26d9ef7fc20fb0a
Mohamed-Amine Fajri <m_h_c@outlook.fr>
2022-05-23 23:50:52 +0200

    Add files via upload

---
+++ ft_erreur.c
@@ -0,0 +1,12 @@
+#include "regulier.h"
+
+void ft_erreur(int a,char *str)
+{
+    if (a == 1)
+        fprintf(stderr, "Problem: nomber argument\n");
+    else if (a == 2)
+        fprintf(stderr, "Problem: l expression donnes n est pas regulier %s\n", str);
+    else if(a == 3)
+        fprintf(stderr, "Problem: l expression regulier ne peut pas se converter en NFA %s\n", str);
+    exit(0);
+}
